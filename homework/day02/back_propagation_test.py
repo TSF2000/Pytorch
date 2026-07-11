48af9a2365c2454ade5267d5f02d4d985956aaa2
Aura <641359853@qq.com>
2026-07-11 16:25:47 +0800

    refactor: 尝试对反向传播代码进行绘图

--- homework/day02/back_propagation_test.py
+++ homework/day02/back_propagation_test.py
@@ -1,4 +1,5 @@
 import torch
+import matplotlib.pyplot as plt
 
 # 准备数据
 x_data = [1.0, 2.0, 3.0]
@@ -18,17 +19,26 @@ def forward(x):
 def loss(x, y):
     return (forward(x) - y) ** 2
 
+epoch_list = []
+loss_list = []
+
 
 print("predict (before training): )", 4, forward(4).item())
 
 # 训练
-for epoch in range(100):
+for epoch in range(200):
     for x, y in zip(x_data, y_data):
         l = loss(x, y)
         l.backward()  # 自动计算梯度
         print("\tgrad:", x, y, w.grad.item())
-        w.data = w.data - 0.01 * w.grad.data
+        w.data = w.data - 0.001 * w.grad.data
         w.grad.data.zero_()
-
+    epoch_list.append(epoch)
+    loss_list.append(l.item())
     print("progress:", epoch, l.item())
 print("predict(after training): ", 4, forward(4).item())
+
+plt.plot(epoch_list, loss_list)
+plt.xlabel("epoch")
+plt.ylabel("loss")
+plt.show()
