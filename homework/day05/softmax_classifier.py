84bbbe216b7d1fc19b01006d1b325043aa6520ff
Aura <641359853@qq.com>
2026-07-14 18:11:21 +0800

    feat: 多分类MNIST数据集训练测试结果绘图

--- homework/day05/softmax_classifier.py
+++ homework/day05/softmax_classifier.py
@@ -1,4 +1,5 @@
 import torch
+import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
@@ -12,31 +13,36 @@ train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 test_dataset = datasets.MNIST(root='./dataset/mnist', train=False, download=True, transform=transform)
 test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
 
+
 class Net(torch.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.l1 = torch.nn.Linear(28*28, 512)
+        self.l1 = torch.nn.Linear(28 * 28, 512)
         self.l2 = torch.nn.Linear(512, 256)
         self.l3 = torch.nn.Linear(256, 128)
         self.l4 = torch.nn.Linear(128, 64)
         self.l5 = torch.nn.Linear(64, 10)
 
     def forward(self, x):
-        x = x.view(-1, 28*28)
+        x = x.view(-1, 28 * 28)
         x = torch.relu(self.l1(x))
         x = torch.relu(self.l2(x))
         x = torch.relu(self.l3(x))
         x = torch.relu(self.l4(x))
         return self.l5(x)
 
+
 model = Net()
 
 criterion = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
 
+
 def train(epoch):
     model.train()
     running_loss = 0.0
+    batch_count = 0
+    total_loss = 0.0
     for batch_idx, (x_data, y_data) in enumerate(train_loader):
         optimizer.zero_grad()
         y_pred = model(x_data)
@@ -45,9 +51,13 @@ def train(epoch):
         optimizer.step()
 
         running_loss += loss.item()
+        total_loss += loss.item()
+        batch_count += 1
         if batch_idx % 300 == 299:
-            print('[%d, %5d] loss: %.3f' %(epoch + 1, batch_idx + 1, running_loss / 300))
+            print('[%d, %5d] loss: %.3f' % (epoch + 1, batch_idx + 1, running_loss / 300))
             running_loss = 0.0
+    return total_loss / batch_count
+
 
 def test():
     model.eval()
@@ -56,12 +66,42 @@ def test():
     with torch.no_grad():
         for (x_data, y_data) in test_loader:
             y_pred = model(x_data)
-            _,predicted = torch.max(y_pred.data, dim=1)
+            _, predicted = torch.max(y_pred.data, dim=1)
             total += y_data.size(0)
-            correct += (predicted==y_data).sum().item()
+            correct += (predicted == y_data).sum().item()
     print('Accuracy of the network on the test images: %d %%' % (100 * correct / total))
+    return 100 * correct / total
+
 
 if __name__ == '__main__':
+    loss_list = []
+    epoch_list = []
+    acc_list = []
     for epoch in range(10):
-        train(epoch)
-        test()
+        avg_loss = train(epoch)
+        loss_list.append(avg_loss)
+        epoch_list.append(epoch)
+        acc = test()
+        acc_list.append(acc)
+
+    fig, ax1 = plt.subplots()
+    color_loss = 'tab:blue'
+    ax1.set_xlabel('epoch')
+    ax1.set_ylabel('loss', color=color_loss)
+    line1 = ax1.plot(epoch_list, loss_list, color=color_loss,label='train loss')
+    ax1.tick_params(axis='y', labelcolor=color_loss)
+
+    ax2 = ax1.twinx()
+    color_acc = 'tab:red'
+    ax2.set_ylabel('accuracy (%)', color=color_acc)
+    line2 = ax2.plot(epoch_list, acc_list, color=color_acc,label='test accuracy')
+    ax2.tick_params(axis='y', labelcolor=color_acc)
+
+    ax1.set_title("Training Loss & Test Accuracy")
+
+    lines = line1 + line2
+    labels = [l.get_label() for l in lines]
+    ax1.legend(lines, labels,loc='best')
+
+    fig.tight_layout()
+    plt.show()
